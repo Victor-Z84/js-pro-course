8fa19ac0f66c913385f03b74a25dfc4b8ca6518f
Victor-Z84 <praguewedphoto@gmail.com>
2021-12-05 21:06:27 +0300

    done work with lint and prettier

--- home-work-5/client-app/index.js
+++ home-work-5/client-app/index.js
@@ -1,12 +1,14 @@
-import isAuth from "./isAuth.js"; // был export default, импорт без {...} 
+import isAuth from "./isAuth.js"; // был export default, импорт без {...}
 
 function getInfo(module, name) {
-    import(module) // загружает модуль и возвращает промис, результат - объект модуля
-    .then ((result) => {
-        let Name = result.default;
-        let user = new Name(name); 
+  import(module) // загружает модуль и возвращает промис, результат - объект модуля
+    .then((result) => {
+      let Name = result.default;
+      let user = new Name(name);
+      /* eslint-disable */
         console.log(user); // вывод в консоль экземпляра класса
-        console.log(`Hello, ${name}!`);
+        /* eslint-enable */
+        console.log(`Hello, ${name}!`); // eslint-disable-line
     });
 }
 
