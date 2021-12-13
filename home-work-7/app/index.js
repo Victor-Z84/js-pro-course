c18b550fb527daf149378f1e9df8f3c9cd335009
Victor-Z84 <praguewedphoto@gmail.com>
2021-12-13 17:40:18 +0300

    done home-work-8--Webpack-part2

--- home-work-7/app/index.js
+++ home-work-7/app/index.js
@@ -1,3 +1,4 @@
+// import { VERSION } from "lodash";
 import { sum } from "./src/calculator.js";
 import { User } from "./src/user/User.js";
 
@@ -10,4 +11,7 @@ import "./styles/style.scss";
 // const bob = new User("Bob");
 // bob.sayHello();
 
-console.log(sum(5)(10));
+console.log(process.env.BASE_API_URL);
+console.log(VERSION);
+
+// console.log(sum(5)(10));
--- home-work-7/webpack.config.js
+++ home-work-7/webpack.config.js
@@ -1,5 +1,9 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require('dotenv-webpack');
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
     entry: "./app/index.js",
@@ -7,8 +11,9 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "index.js"
     },
+    target: ['web', 'es5'],
     // mode: "production",
-    mode: "development",
+    // mode: "development",
 
     module: {
         rules: [
@@ -32,5 +37,18 @@ module.exports = {
             title: "Home Work 7",
             template: "./app/index.html"
         }),
+        new Dotenv(),
+        new webpack.DefinePlugin({
+            PRODUCTION: JSON.stringify(true),
+            VERSION: JSON.stringify('5fa3b9'),
+            BROWSER_SUPPORTS_HTML5: true,
+            TWO: '1+1',
+        }),
+        new CopyPlugin({
+            patterns: [
+              { from: "./app/assets", to: "./assets" },
+            ],
+        }),
+        new CleanWebpackPlugin(),
     ]
 }
